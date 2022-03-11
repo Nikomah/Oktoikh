b065178996a5344459c37a237adcbab4b33dacde
Nikomah <d.nikomah@yandex.ru>
2022-03-11 13:20:38 +0300

    Reducing the menu in the mobile version.

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,19 +1,7 @@
-"""
-WSGI config for HelloDjango project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
-"""
-
+# -*- coding: utf-8 -*-
 import os, sys
+sys.path.insert(0, '/home/o/octoechos/octoechos.beget.tech/my_own_site')
+sys.path.insert(1, '/home/o/octoechos/.local/lib/python3.10/site-packages')
+os.environ['DJANGO_SETTINGS_MODULE'] = '<название_проекта>.settings'
 from django.core.wsgi import get_wsgi_application
-
-site_user_root_dir = '/home/o/octoechos/octoechos.beget.tech/public_html'
-sys.path.insert(0, os.path.join(site_user_root_dir, 'my_own_site'))
-sys.path.insert(1, os.path.join(site_user_root_dir, 'venv/lib/python3.6/site-packages'))
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_own_site.settings')
-
 application = get_wsgi_application()
